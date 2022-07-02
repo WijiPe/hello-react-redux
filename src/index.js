384e966fea7cb18c644a321bbbd3945cb7d75dc7
Wiji <peawseaw@gmail.com>
2022-07-01 21:40:59 -0700

    set up provider

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import rootReducer from '.reducers';
+
+const store = createStore(rootReducer);
+
+const MyApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
