99fc5cb90fa00fddaa609131bc0622204fad0d7f
Wiji <peawseaw@gmail.com>
2022-07-02 00:04:19 -0700

    redux logger

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import { increment, decrement } from './actions'
+
+const App = ({ message, counter, dispatch  }) => (
+  <div className="container">
+    <div className="columns column is-12">
+      <h1>Counter : {counter}</h1>
     </div>
-  );
-}
 
-export default App;
+    <div className="buttons">
+      <button
+        onClick={() => dispatch(increment(1))}
+        className="button is-primary"
+      >
+        +1
+      </button>
+      <button onClick={() => dispatch(increment(2))} className="button is-link">
+        +2
+      </button>
+      <button onClick={() => dispatch(increment(3))} className="button is-info">
+        +3
+      </button>
+    </div>
+
+    <div className="buttons">
+      <button
+        onClick={() => dispatch(decrement(1))}
+        className="button is-primary"
+      >
+        -1
+      </button>
+      <button onClick={() => dispatch(decrement(2))} className="button is-link">
+        -2
+      </button>
+      <button onClick={() => dispatch(decrement(3))} className="button is-info">
+        -3
+      </button>
+    </div>
+  </div>
+);
+
+const mapStateToProps = function (state) {
+  return {
+    message: 'This is message from mapStateToProps',
+    counter: state.counters || 0
+  };
+};
+
+const AppWithConnect = connect(mapStateToProps)(App);
+export default AppWithConnect;
+
